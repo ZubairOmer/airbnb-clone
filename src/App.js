a4d3cc562792a1c4d29da0c61d62abba9561752f
Zubair Omer <omer.zubair01@gmail.com>
2021-06-05 06:15:30 -0700

    react-router setup applied

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import SearchPage from './SearchPage'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='app'>
       <Header />
-      <Home />
+      <Switch>
+        <Route exact path='/search' component={SearchPage} />
+        <Route exact path='/' component={Home} />
+      </Switch>
       <Footer />
     </div>
   )
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>@ 2021 Airbnb clone developed by Zubair Omer</p>
+        </div>
+    )
+}
+
+export default Footer
