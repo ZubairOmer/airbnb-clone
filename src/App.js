dafe567dc9e10f98ad1d877c3b29538f4af52c0e
Zubair Omer <omer.zubair01@gmail.com>
2021-06-05 10:56:58 -0700

    minor bugs fixed

--- src/App.js
+++ src/App.js
@@ -5,27 +5,17 @@ import Header from './Header'
 import Footer from './Footer'
 import SearchPage from './SearchPage'
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-
-    // BEM
     <div className="app">
-      <Router>
-        <Header />
-        
-        <Switch>
-          <Route path="/search">
-            <SearchPage />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        
-        <Footer />
-      </ Router>
+      <Header />
+      <Switch>
+        <Route exact path='/search' component={SearchPage} />
+        <Route exact path='/' component={Home} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,7 @@ import './Footer.css'
 function Footer() {
     return (
         <div className='footer'>
-            <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
-            <p>Privacy · Terms · Sitemap · Company Details</p>
+            <p>© 2021 Airbnb Clone developed by Zuabir Omer</p>
         </div>
     )
 }
