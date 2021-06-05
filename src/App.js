4ea067d94387c61b61d739bf84856bf717a3ce08
Zubair Omer <omer.zubair01@gmail.com>
2021-06-05 05:41:23 -0700

    home component with all its sub components DONE!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className='app'>
       <Header />
       <Home />
+      <Footer />
     </div>
   )
 }
